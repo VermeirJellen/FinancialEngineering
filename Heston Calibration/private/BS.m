328f709eb284f2826de97a0af32f785349df93b2
Jellen Vermeir <jellenvermeir@gmail.com>
2015-08-10 03:46:34 +0200

    Corrections to readme files. Adding LGPL licensing.

--- Heston Calibration/private/BS.m
+++ Heston Calibration/private/BS.m
@@ -1,3 +1,20 @@
+%	Copyright 2015 Jellen Vermeir 
+%	jellenvermeir@gmail.com	
+
+% This file is part of the Financial Engineering Toolkit
+%
+% Financial Engineering Toolkit is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% at your option) any later version.
+%
+% Financial Engineering Toolkit is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU Lesser General Public License
+% along with Financial Engineering Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 function priceBS = BS(sigma, S0, K, r, q, T, flag) 
 
 d_1 = (log(S0./K)+(r - q +sigma.^2./2).*T)./(sigma.*sqrt(T));
--- Heston Calibration/private/MinimizationCriterionHeston.m
+++ Heston Calibration/private/MinimizationCriterionHeston.m
@@ -1,3 +1,20 @@
+%	Copyright 2015 Jellen Vermeir 
+%	jellenvermeir@gmail.com	
+
+% This file is part of the Financial Engineering Toolkit
+%
+% Financial Engineering Toolkit is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% at your option) any later version.
+%
+% Financial Engineering Toolkit is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+%
+% You should have received a copy of the GNU Lesser General Public License
+% along with Financial Engineering Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 function minimizationCriterionHeston = MinimizationCriterionHeston(paramsCalibration,K,T,S0,r,q,optionType,CarrMadanPrecomputed,optionMidMarket,weights)
 
 kappa = (paramsCalibration(1)+paramsCalibration(3)^2)/(2*paramsCalibration(2)); % extract kappa from fellerCondition
